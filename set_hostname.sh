0e0bbc15599d1ee815f842eb24b6e5a4d66dd23c
Davis Haupt <davis.haupt@gmail.com>
2022-10-01 10:41:28 -0400

    set hostname script

---
+++ set_hostname.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+hostline=$(flyctl status | grep Hostname)
+if [[ $? -ne 0 ]]; then
+    echo "$0: flyctl status exited with an error. Please fix that error before continuing."
+    exit;
+fi
+found_hostname=$(echo $hostline | grep -c Hostname)
+if [ "$found_hostname" != "1" ]; then
+    echo "$0: hostname not found in output for flyctl status. Exiting setup script."
+    exit;
+fi
+
+hostname=$(echo $hostline | grep Hostname | cut -d '=' -f 2 | xargs)
+
+sed -i '' -e "s/<FLY_HOSTNAME>/$hostname/g" */settings.py
